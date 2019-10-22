9ed05e73e04fd77931e77be8f20565d90e56ac04
Resilient Coders 7 <resilientcoders7@Resilients-MacBook-Pro.local>
2019-10-22 09:11:50 -0400

    codeWarTwo

---
+++ dailyCodeTwo.js
@@ -0,0 +1,7 @@
+// Complete the solution so that it reverses the string value passed into it.
+// solution('world'); // returns 'dlrow'
+// Test.expect(solution('world') == 'dlrow')
+
+function solution(str){
+return str.split("").reverse().join("")
+}
