bd4deb987325fad6e60bba730785e59071e2f9e5
Resilient Coders 7 <mauricio.acosta92@gmail.com>
2019-10-22 09:42:29 -0400

    dailyCodeWars

--- dailyCodeTwo.js
+++ dailyCodeTwo.js
@@ -1,3 +1,4 @@
+// Daily Code Wars
 // Complete the solution so that it reverses the string value passed into it.
 // solution('world'); // returns 'dlrow'
 // Test.expect(solution('world') == 'dlrow')
