42a6b0ffbc8f82236f3ad7914993d74f71b110bc
Resilient Coders 7 <mauricio.acosta92@gmail.com>
2019-10-22 19:51:38 -0400

    added daily code wars

--- dailyCodeWars.js
+++ dailyCodeWars.js
@@ -1,3 +1,4 @@
+//Daily Code Wars
 //CODE WAR 1
 // Your friend advised you to see a new performance in the most popular theater in the city. He knows a lot about art and his advice is usually good, but not this time: the performance turned out to be awfully dull. It's so bad you want to sneak out, which is quite simple, especially since the exit is located right behind your row to the left. All you need to do is climb over your seat and make your way to the exit.
 //
