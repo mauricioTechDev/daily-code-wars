7af4f77cb4c30f08fea63c921fafa22d2767149b
Resilient Coders 7 <mauricio.acosta92@gmail.com>
2019-11-04 18:54:42 -0500

    code wars

---
+++ Count-the-Monkeys.js
@@ -0,0 +1,13 @@
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+
+
+function monkeyCount(n) {
+// your code here
+let array = []
+for(let i=1; i<=n; i++){
+array.push(i)
+}
+return array
+}
