df9067c7f06799977e9d4f9215858686c0346adc
Resilient Coders 7 <mauricio.acosta92@gmail.com>
2019-11-08 08:56:42 -0500

    daily code war

---
+++ get-character-from-ASCII-Value.js
@@ -0,0 +1,9 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+// Example:
+// get_char(65) # => 'A'
+
+
+function getChar(c){
+return String.fromCharCode(c)
+  // ...
+}
