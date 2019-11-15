f9a3a8dbaaa2207f4a9e4e0c796663ba63efe3b5
Resilient Coders 7 <mauricio.acosta92@gmail.com>
2019-11-15 18:25:18 -0500

    daily code war

---
+++ Sentence-to-words.js
@@ -0,0 +1,11 @@
+// Write function splitSentence which will create a list of strings from a string.
+//
+// Example:
+//
+// "hello world" -> ["hello", "world"]
+
+
+function splitSentence(s) {
+let array = s.split(" ")
+  return array;
+}
