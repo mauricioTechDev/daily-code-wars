6e123deeadde7da917f3a42b9f1d4fedef45f2b3
Resilient Coders 7 <mauricio.acosta92@gmail.com>
2019-11-17 09:04:51 -0500

    daily code wars

---
+++ shoppingList.js
@@ -0,0 +1,24 @@
+// Instructions:
+// You are in charge of ordering food for a party. You are going to need 4 sandwiches, 6 salads, 5 wraps, and 10 orders of french fries. The cost per item of food is:
+// food	price
+// sandwich	$8.00
+// salad	$7.00
+// wrap	$6.50
+// french fries	$1.20
+// Create 4 variables to store the quantity of each type of food with the following names:
+// * sandwiches
+// * salads
+// * wraps
+// * frenchFries
+//
+// Create a variable named totalPrice that finds the cost of all of the food.
+
+var sandwiches = 4
+var sandPrice =  sandwiches*8
+var salads = 6
+var saladPrice = salads*7
+var wraps = 5
+var wrapPrice = wraps*6.50
+var frenchFries = 10
+var frenchPrice = frenchFries*1.20
+var totalPrice = frenchPrice+wrapPrice+saladPrice+sandPrice
