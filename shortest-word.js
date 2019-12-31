693f72f24cb98e44d8c02e91f6cdd40195ceb9e1
Mauricio Acosta <mauricio.acosta92@gmail.com>
2019-12-30 22:59:02 -0500

    daily code war

--- shortest-word.js
+++ shortest-word.js
@@ -1,6 +1,6 @@
-// Simple, given a string of words, return the length of the shortest word(s).
+// Simple, given a string of words, return the length of the shortest word(s)
 
-// String will never be empty and you do not need to account for different data types.
+// String will never be empty and you do not need to account for different data types
 
 function findShort(s) {
 	let arr = s.split(' ');
