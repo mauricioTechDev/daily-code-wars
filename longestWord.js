d518ab68ca9f1b6b8a007aa561bee1be1dc31780
resilient-new07 <mauricio.acosta92@gmail.com>
2020-01-22 17:24:58 -0500

    daily code war

---
+++ longestWord.js
@@ -0,0 +1,18 @@
+// fidn the longest word
+
+
+function findLongest(str) {
+
+  var spl = str.split(" ");
+  console.log(spl)
+  var longest = 0
+
+  for (var i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest) {
+      longest = spl[i].length
+    }
+
+    }
+    return longest
+
+}
