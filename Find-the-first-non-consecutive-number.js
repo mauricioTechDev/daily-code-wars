c5a4480577596f372d9bf97038d548fa16c60773
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-01-27 22:52:28 -0500

    clean up

--- Find-the-first-non-consecutive-number.js
+++ Find-the-first-non-consecutive-number.js
@@ -5,11 +5,7 @@
 // If the whole array is consecutive then return null or Nothing.
 //
 // The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
-//
-// If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
-//
 // 1) Can you write a solution that will return null for both [] and [ x ] though? ( This is not tested, but you can write your own example test. )
-//
 
 function firstNonConsecutive(arr) {
   for (let i = 0; i < arr.length - 1; ++i) {
