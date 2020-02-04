436880b5b20870b613376d9c0a3b9ee2b7a21dd6
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-02-04 15:10:06 -0500

    edit code

--- balancedBracket.js
+++ balancedBracket.js
@@ -62,22 +62,16 @@ let isMatchingBrackets = function (str) {
     }
 
     for (let i = 0; i < str.length; i++) {
-
-        // If character is an opening brace add it to a stack
         if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
             stack.push(str[i]);
 //                     console.log(stack)
 
-        }
-        //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
-        else {
+        } else {
             let last = stack.pop();
                     console.log(stack)
                     console.table(last)
                     console.log(map[last])
 
-
-            //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
             if (str[i] !== map[last]) {
               console.log(str[i]+"hello")
               console.log(map[last]+"ohShit")
@@ -85,7 +79,6 @@ let isMatchingBrackets = function (str) {
             };
         }
     }
-    // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
         if (stack.length !== 0) {
           return false
         };
