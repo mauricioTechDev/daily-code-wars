06ba4dcc2e86638706753f744a88247575f479df
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-02-23 14:28:14 -0500

    daily code war

---
+++ defanging-an-ip-address.js
@@ -0,0 +1,27 @@
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+
+
+
+
+var defangIPaddr = function(address) {
+    let arr = address.split('')
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] == "."){
+            arr.splice(i, 1, "[.]")
+        }
+    }
+    return arr.join('')
+};
