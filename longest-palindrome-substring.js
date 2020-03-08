0b74cac7bcbd0e936ae30564a16260037dd5f719
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-03-08 17:30:23 -0400

    edit to make cleared and more modulized

--- longest-palindrome-substring.js
+++ longest-palindrome-substring.js
@@ -42,3 +42,31 @@ var expandAroundCenter = function (s, left, right) {
 };
 // Time complexity : O(n^2).
 // Space complexity : O(1).
+var longestPalindrome = function(s) {
+  var start = 0;
+  var end = 0;
+  var len = s.length;
+  for (var i = 0; i < len; i++) {
+      let len1 = expandAroundCenter(s, i, i);
+      let len2 = expandAroundCenter(s, i, i+1);
+      let maxLength = Math.max(len1, len2)
+    if (maxLength > end - start) {
+      start = i - Math.floor((maxLength - 1) / 2);
+      end = i + Math.floor(maxLength / 2);
+    }
+  }
+  return s.slice(start, end + 1);
+};
+
+var expandAroundCenter = function (s, left, right) {
+  var l = left;
+  var r = right;
+  var len = s.length;
+  while (l >= 0 && r < len && s[l] === s[r]) {
+    l--;
+    r++;
+  }
+  return r - l - 1;
+};
+'aabbaa'
+console.log(longestPalindrome(s))
