13bcc9b3de41624d7ab2414797e751d7936fd90b
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-03-22 14:59:41 -0400

    daily code war

---
+++ minimum-index-sum-of-two-list.js
@@ -0,0 +1,41 @@
+Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+
+You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+
+Example 1:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+Output: ["Shogun"]
+Explanation: The only restaurant they both like is "Shogun".
+Example 2:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["KFC", "Shogun", "Burger King"]
+Output: ["Shogun"]
+Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+Note:
+The length of both lists will be in the range of [1, 1000].
+The length of strings in both lists will be in the range of [1, 30].
+The index is starting from 0 to the list length minus 1.
+No duplicates in both lists.
+
+
+
+
+
+var findRestaurant = function(list1, list2) {
+    let sum = Number.POSITIVE_INFINITY;
+    let match = [];
+    for(let i = 0; i<list1.length; i++){
+        for(let j = 0; j<list2.length; j++){
+            if(list1[i] == list2[j]){
+                if(i + j <= sum){
+                 sum = i + j;
+                 match.push(list1[i])
+                }
+            }
+        }
+    }
+    return match
+};
