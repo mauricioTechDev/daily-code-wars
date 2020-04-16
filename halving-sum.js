d8f932a9f585121c26bc677fe74cb6b7faabfcaa
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-04-15 21:02:18 -0400

    daily code war

---
+++ halving-sum.js
@@ -0,0 +1,13 @@
+Given a positive integer n, calculate the following sum:
+
+n + n/2 + n/4 + n/8 + ...
+All elements of the sum are the results of integer division.
+
+Example
+25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+
+
+function halvingSum(n) {
+    return n === 1 ? n : n + halvingSum(Math.floor(n/2))
+}
