b2fe889dd200743670ac423ae50b6e339ba5d45e
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-04-27 13:44:04 -0400

    daily code war

---
+++ palindrome-index.js
@@ -0,0 +1,76 @@
+Given a string of lowercase letters in the range ascii[a-z], determine a character that can be removed to make the string a palindrome. There may be more than one solution, but any will do. For example, if your string is "bcbc", you can either remove 'b' at index  or 'c' at index . If the word is already a palindrome or there is no solution, return -1. Otherwise, return the index of a character to remove.
+
+Function Description
+
+Complete the palindromeIndex function in the editor below. It must return the index of the character to remove or .
+
+palindromeIndex has the following parameter(s):
+
+s: a string to analyze
+Input Format
+
+The first line contains an integer , the number of queries.
+Each of the next  lines contains a query string .
+
+Constraints
+
+All characters are in the range ascii[a-z].
+Output Format
+
+Print an integer denoting the zero-indexed position of the character to remove to make  a palindrome. If  is already a palindrome or no such character exists, print .
+
+Sample Input
+
+3
+aaab
+baa
+aaa
+Sample Output
+
+3
+0
+-1
+Explanation
+
+Query 1: "aaab"
+Removing 'b' at index  results in a palindrome, so we print  on a new line.
+
+Query 2: "baa"
+Removing 'b' at index  results in a palindrome, so we print  on a new line.
+
+Query 3: "aaa"
+This string is already a palindrome, so we print . Removing any one of the characters would result in a palindrome, but this test comes first.
+
+Note: The custom checker logic for this challenge is available here.
+
+
+
+
+function pali(s){
+  if(helper(s)){return -1}
+  for(let i = 0; i<s.length; i++){
+    let newStr = str.slice(0,i) + str.slice(i+1)
+
+    if(isPali(newStr)){
+      return i
+    }
+  }
+  return -1
+
+}
+
+function helper(str){
+
+   let leftPointer = 0
+    let rightPointer = str.length - 1
+  while(leftPointer < rightPointer){
+
+        if(str[leftPointer] !== str[rightPointer]){
+
+           return false
+        }
+        leftPointer++
+        rightPointer--
+    }
+  return true
+}
