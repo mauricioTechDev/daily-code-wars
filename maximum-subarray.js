151dc3877f4fa3d4e76ceaec93233b3110fcc1fe
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-06-02 12:15:02 -0400

    comment code war

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -1,56 +1,38 @@
-// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-//
-// Example:
-//
-// Input: [-2,1,-3,4,-1,2,1,-5,4],
-// Output: 6
-// Explanation: [4,-1,2,1] has the largest sum = 6.
-// Follow up:
-//
-// If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
-//
-//
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 
 
-
+// time  O(n^2) space O(n^2) brute force solution
 var maxSubArray = function(nums) {
+  let sum = num[0]
 
-  // time  O(n^2)
-// let leftIndex = 0
-// let rightIndex =nums.length - 1
-// var max_so_far = Number.NEGATIVE_INFINITY;
+  for(let i = 0; i<nums.length; i++){
+      for(let j = i; j<nums.length; j++){
+          let currentSum = nums.sclice(i, j+1).reduce((acc,curr) => acc, curr, 0)
+          if(currentSum > sum){
+                  sum = currentSum
+          }
+      }
+  }
+}
 
-// for(let i = 0; i<nums.length; i++){
-//      maxSum = 0
-//     for(let j = i; j<nums.length; j++){
-//         maxSum += nums[j]
-//         if(max_so_far<maxSum){
-//                 leftIndex = i;
-// 				max_so_far = maxSum;
-// 				rightIndex = j;
-//         }
-//     }
-// }
-//     return {
-// 		left: leftIndex,
-// 		right: rightIndex,
-// 		final_max_sum_subArray: max_so_far
-// 	};
+
+
 
 
 
 // time  O(n), space O(1)
-    var curr_max = Number.NEGATIVE_INFINITY, max_so_far = Number.NEGATIVE_INFINITY;
-
-  for(var i = 0; i < nums.length; i++){
-    curr_max = Math.max(nums[i], curr_max + nums[i]);
-    max_so_far = Math.max(curr_max, max_so_far);
-  }
-  return max_so_far;
-///////////////////////////////////////////////
-Refactored solution using Kadanes algorithm using dynamic programing
-In linear time and constant space
-
+// Refactored solution using Kadanes algorithm using dynamic programing
+// In linear time and constant space
+var maxSubArray = function(nums) {
   let maxSoFar = nums[0];
    let maxEndingHere = nums[0];
 
