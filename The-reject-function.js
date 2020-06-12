ed04b39bce3821bd50546c52118c7df2bfbfa0a2
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-06-12 14:03:47 -0400

    daily code war

---
+++ The-reject-function.js
@@ -0,0 +1,15 @@
+Implement a function which filters out array values which satisfy the given predicate.
+
+reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
+
+
+function reject(array, predicate) {
+  //
+  let arr = []
+  for(let i = 0; i < array.length; i++){
+    if(!predicate(array[i])){
+      arr.push(array[i])
+    }
+  }
+  return arr
+}
