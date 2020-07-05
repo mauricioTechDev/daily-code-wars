467fdd0fb504fccaae9f0746fc8f6519fae776a3
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-07-05 09:10:12 -0400

    daily code war

---
+++ shortestWord.js
@@ -0,0 +1,19 @@
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+Test.describe("Example tests",_=>{
+Test.assertEquals(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+Test.assertEquals(findShort("turns out random test cases are easier than writing out basic ones"), 3);
+});
+
+function findShort(s){
+  let arr  = s.split(' ');
+  let smallest = arr[0].length;
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].length <= smallest){
+      smallest = arr[i].length
+    }
+  }
+  return smallest;
+}
