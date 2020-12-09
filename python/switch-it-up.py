5f2702d02cad48b164281e214f9ca167502406e4
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-12-08 21:05:44 -0500

    Create switch-it-up.py
    python code yeaaaa

---
+++ python/switch-it-up.py
@@ -0,0 +1,24 @@
+# When provided with a number between 0-9, return it in words.
+
+#  Input :: 1
+
+#  Output :: "One".
+
+#  If your language supports it, try using a switch statement.
+
+def switch_it_up(number):
+    #your code here
+    dictionary = {
+        0: 'Zero',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine'
+    }
+    return dictionary[number]
+    
