0fe3c9ed5b82eaed175460295eea187f8c3dc8db
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-12-17 07:37:19 -0500

    Create filling-an-array-1.py
    python code

---
+++ python/filling-an-array-1.py
@@ -0,0 +1,11 @@
+# We want an array, but not just any old array, an array with contents!
+
+# Write a function that produces an array with the numbers 0 to N-1 in it.
+
+# For example, the following code will result in an array containing the numbers 0 to 4:
+
+# arr(5) // => [0,1,2,3,4]
+
+def arr(n=0):    #To handle a call without an argument, e.g. arr(), default arg to 0
+    return list(range(n))
+    
