6b81f7f600d7a4d2a8bdf7678069fddd44d3b357
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-12-22 01:50:55 -0500

    Create check-the-exam.py

---
+++ python/check-the-exam.py
@@ -0,0 +1,26 @@
+# The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+# The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+# If the score < 0, return 0.
+
+# For example:
+
+# checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+# checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+# checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+# checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+
+
+def check_exam(arr1,arr2):
+    score = 0
+
+    for i in range(len(arr1)):
+        if arr1[i] == arr2[i]:
+            score+=4
+        elif arr2[i] == '':
+            score+=0
+        else:
+            score-=1
+    return 0 if score < 0 else score
