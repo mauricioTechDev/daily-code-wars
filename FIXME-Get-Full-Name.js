c27974c0afaacbda6652045910a955468c4ae3b8
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-01-31 18:11:01 -0500

    Update FIXME-Get-Full-Name.js
    add before code

--- FIXME-Get-Full-Name.js
+++ FIXME-Get-Full-Name.js
@@ -1,13 +1,26 @@
-The code provided is supposed return a person's Full Name given their first and last names.
+//The code provided is supposed return a person's Full Name given their first and last names.
 
-But it's not working properly.
+//But it's not working properly.
 
-Notes
-The first and/or last names are never null, but may be empty.
+//Notes
+//The first and/or last names are never null, but may be empty.
 
-Task
-Fix the bug so we can all go home early.
+//Task
+//Fix the bug so we can all go home early.
 
+//BEFORE
+class Dinglemouse{
+
+  constructor( firstName, lastName ){
+  }
+  
+  getFullName(){
+    return firstName+" "+lastName
+  }
+  
+}
+
+//AFTER
 class Dinglemouse{
 
   constructor( firstName, lastName ){
