ce6b1e7bda156cf751e62c0110bf7cf052a238b2
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-02-09 22:23:39 -0500

    Create Subdomain-Visit-Count.js
    Subdomain-Visit-Count

---
+++ leetcodes/Subdomain-Visit-Count.js
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/subdomain-visit-count/
+
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+var subdomainVisits = function(cpdomains) {
+     let visitCounts = {};
+    for (let i = 0; i < cpdomains.length; i++) {
+        // Split visits and domains by the space
+        const [visits, domains] = cpdomains[i].split(" ");
+        
+        // Create array of subdomains in domain
+        let subdomains = domains.split(".");
+        
+        while(subdomains.length) {
+            // Join all items in subdomains array
+            let subdomain = subdomains.join('.');
+            
+            // If subdomain already exists in object, add to existing count
+            visitCounts[subdomain] = visitCounts.hasOwnProperty(subdomain) ?
+                Number(visitCounts[subdomain]) + Number(visits) :
+                visits;
+            
+            // Remove first subdomain from array
+            subdomains.shift();
+        }
+    }
+
+    return Object.keys(visitCounts).map((key) => `${visitCounts[key]} ${key}`);
+};
